c302edeaeb9090eab4a91487518c4444c7244ee4
Bulat Kurbangaliev <8617623+ilov3@users.noreply.github.com>
2024-12-28 15:13:56 +0300

    fix os check

--- zsh/.hosts/MAC1042OKKO.sh
+++
@@ -1,1 +0,0 @@
-export PATH=$PATH:"/Applications/PyCharm 2023.3 EAP.app/Contents/MacOS:/Applications/DataGrip 2023.3 EAP.app/Contents/MacOS"
--- zsh/.util.sh
+++ zsh/.util.sh
@@ -6,56 +6,56 @@ force_stow() {
 }
 
 is_linux() {
-  if [ "$(uname -s)" == "Linux" ];
+  if [ "$(uname -s | tr '[:upper:]' '[:lower:]')" == "linux" ];
   then return 0
   else return 1
   fi
 }
 
 is_fedora() {
-  if uname -a | grep fedora > /dev/null;
+  if uname -a | tr '[:upper:]' '[:lower:]' | grep fedora > /dev/null;
   then return 0
   else return 1
   fi
 }
 
 is_ubuntu() {
-  if uname -a | grep ubuntu > /dev/null;
+  if uname -a | tr '[:upper:]' '[:lower:]' | grep ubuntu > /dev/null;
   then return 0
   else return 1
   fi
 }
 
 is_mac() {
-  if [ "$(uname -s)" == "Darwin" ];
+  if [ "$(uname -s | tr '[:upper:]' '[:lower:]')" == "darwin" ];
   then return 0
   else return 1
   fi
 }
 
 is_linux_64() {
-  if [ "$(uname -s)" == "Linux" ] && [ "$(arch)" == "x86_64" ];
+  if [ "$(uname -s | tr '[:upper:]' '[:lower:]')" == "linux" ] && [ "$(arch)" == "x86_64" ];
   then return 0
   else return 1
   fi
 }
 
 is_linux_arm() {
-  if [ "$(uname -s)" == "Linux" ] && [ "$(arch)" == "aarch64" ];
+  if [ "$(uname -s | tr '[:upper:]' '[:lower:]')" == "linux" ] && [ "$(arch)" == "aarch64" ];
   then return 0
   else return 1
   fi
 }
 
 is_mac_64() {
-  if [ "$(uname -s)" == "Darwin" ] && [ "$(arch)" == "i386" ];
+  if [ "$(uname -s | tr '[:upper:]' '[:lower:]')" == "darwin" ] && [ "$(arch)" == "i386" ];
   then return 0
   else return 1
   fi
 }
 
 is_mac_arm() {
-  if [ "$(uname -s)" == "Darwin" ] && [ "$(arch)" == "aarch64" ];
+  if [ "$(uname -s | tr '[:upper:]' '[:lower:]')" == "darwin" ] && [ "$(arch)" == "aarch64" ];
   then return 0
   else return 1
   fi
